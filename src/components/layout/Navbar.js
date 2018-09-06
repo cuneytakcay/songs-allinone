22755f78434c90231a53ce3319775dfb7bad9bf5
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-06 01:44:55 -0700

    bootswatch, components folder and Navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+	return (
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<a class="navbar-brand" href="#">Songs AIO</a>
+		</nav>
+	)
+}
+
+export default Navbar
