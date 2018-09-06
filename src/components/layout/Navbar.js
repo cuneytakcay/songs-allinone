81a005f9a467167e31f5a9c0498bade307545ca2
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-06 01:48:04 -0700

    class to className in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-			<a class="navbar-brand" href="#">Songs AIO</a>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+			<div className="container">
+				<a className="navbar-brand" href="#">Songs AIO</a>
+			</div>
 		</nav>
 	)
 }
