127cca9963f7375d7ed285260ddbd281e19c1ff2
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-08 14:05:43 -0700

    album cover

---
+++ src/components/songs/Album.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+import noimage from './no-cover-art.png'
+
+class Album extends Component {
+	state = {
+		artist: this.props.artist.replace('&', 'and'),
+		album: this.props.album.replace('&', 'and'),
+		imageURL: '',
+	}
+
+	componentDidMount() {
+		axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.REACT_APP_LAST_FM_KEY}&artist=${this.state.artist}&album=${this.state.album}&format=json`)
+			.then(res => {
+				this.setState({ imageURL: res.data.album })	
+			})
+			.catch(err => console.log(err))
+	}
+
+	render() {
+		if (this.state.imageURL && this.state.imageURL.image[3]['#text'] !== '') {
+			return (<img src={this.state.imageURL.image[3]['#text']} alt={this.state.album} className="w-100" />)
+		} else {
+			return (<img src={noimage} alt={this.state.album} className="w-100" />)
+		}
+	}
+}
+
+export default Album
--- src/components/songs/Song.js
+++ src/components/songs/Song.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Album from './Album'
+
 const Song = (props) => {
 	const { song } = props
 
 	return (
 		<div className="col-md-6">
 			<div className="card mb-3 shadow-sm">
-				<div className="card-body">
-					<h5>{song.artist_name}</h5>
-					<p className="card-text">
-						<i className="fas fa-play text-primary"></i> <strong>Song</strong>: {song.track_name}
-						<br />
-						<i className="fas fa-compact-disc text-primary"></i> <strong>Album</strong>: {song.album_name}
-					</p>
+				<div className="card-body row">
+					<div className="col-lg-6">
+						<Album artist={song.artist_name} album={song.album_name} />
+					</div>
+					<div className="col-lg-6 pt-2">
+						<h5>{song.artist_name}</h5>
+						<p className="card-text">
+							<i className="fas fa-play text-primary"></i> <strong>Song</strong>: {song.track_name}
+							<br />
+							<i className="fas fa-compact-disc text-primary"></i> <strong>Album</strong>: {song.album_name}
+						</p>
+					</div>
+				</div>
+				<div className="pl-3 pr-3 pb-3">
 					<Link to={`lyrics/song/${song.track_id}`} className="btn btn-dark btn-block">
 						<i className="fas fa-chevron-right text-primary"></i> View Lyrics
 					</Link>
