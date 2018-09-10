382451ddc0dd70b65d17480033310ee8bf9036c0
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-10 10:33:24 -0700

    data url variables

--- src/components/songs/Album.js
+++ src/components/songs/Album.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-import noimage from '../img/no-cover-art.png'
+import noimage from '../img/no-cover-art.png';
+
+const rootURL = `http://ws.audioscrobbler.com/2.0/`;
+const key = process.env.REACT_APP_LAST_FM_KEY;
 
 class Album extends Component {
 	state = {
-		artist: this.props.artist.replace('&', 'and'),
-		album: this.props.album.replace('&', 'and'),
+		artist: this.props.artist,
+		album: this.props.album,
 		imageURL: '',
-	}
+	};
 
 	componentDidMount() {
-		axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${process.env.REACT_APP_LAST_FM_KEY}
-			&artist=${this.state.artist}&album=${this.state.album}&format=json`)
+		axios.get(`${rootURL}?method=album.getinfo&api_key=${key}&artist=${this.state.artist}&album=${this.state.album}&format=json`)
 			.then(res => {
-				this.setState({ imageURL: res.data.album })	
+				this.setState({ imageURL: res.data.album })	;
 			})
-			.catch(err => console.log(err))
+			.catch(err => console.log(err));
 	}
 
 	render() {
@@ -26,13 +28,13 @@ class Album extends Component {
 				<Link to={`/album/${this.state.artist}/${this.state.album}`}>
 					<img src={this.state.imageURL.image[3]['#text']} alt={this.state.album} className="w-100" />
 				</Link>
-			)
+			);
 		} else {
 			return (
 				<img src={noimage} alt={this.state.album} className="w-100" />
-			)
+			);
 		}
 	}
 }
 
-export default Album
+export default Album;
