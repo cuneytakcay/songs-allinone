9c44f542500430065a6fba1e3229cc6241c2abd3
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-10 10:02:54 -0700

    lyrics data url changed

--- src/pages/Lyrics.js
+++ src/pages/Lyrics.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Moment from 'react-moment'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 
-import Spinner from '../components/layout/Spinner'
+import Spinner from '../components/layout/Spinner';
+
+const cors = `https://cors-anywhere.herokuapp.com/`;
+const rootURL = `http://api.musixmatch.com/ws/1.1/`;
+const key = process.env.REACT_APP_MUSIXMATCH_KEY;
 
 class Lyrics extends Component {
   state = {
-    song: {},
+    track: {},
     lyrics: {},
   }
 
   componentDidMount() {
-    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}
-      &apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`)
+    axios.get(`${cors}${rootURL}track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${key}`)
       .then(res => {
-        this.setState({
-          lyrics: res.data.message.body.lyrics
-        })
+        this.setState({ lyrics: res.data.message.body.lyrics })
         return (
-          axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}
-            &apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`)
+          axios.get(`${cors}${rootURL}track.get?track_id=${this.props.match.params.id}&apikey=${key}`)
             .then(res => {
-              this.setState({
-                song: res.data.message.body.track
-              })
+              this.setState({ track: res.data.message.body.track })
             })
             .catch(err => console.log(err))
         )
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
 
   render() {
-    const { song, lyrics } = this.state
+    const { track, lyrics } = this.state
 
     if (
-      song === undefined || 
+      track === undefined || 
       lyrics === undefined || 
-      Object.keys(song).length === 0 || 
+      Object.keys(track).length === 0 || 
       Object.keys(lyrics).length === 0
     ) {
-      return <Spinner />
+      return (<Spinner />);
     } else {
       return (
         <React.Fragment>
           <Link to="/" className="btn btn-dark btn-sm mb-4 mt-2">Go back</Link>
           <div className="card">
             <h5 className="card-header">
-              {song.track_name} / <span className="text-dark">{song.artist_name}</span>
+              {track.track_name} / <span className="text-dark">{track.artist_name}</span>
             </h5>
             <div className="card-body">
               <pre className="card-text">{lyrics.lyrics_body}</pre>
@@ -57,22 +55,22 @@ class Lyrics extends Component {
           <ul className="list-group mt-3 mb-4">
             <li className="list-group-item">
               <strong>Song Genre</strong>: {
-                song.primary_genres.music_genre_list.length !== 0 ?
-                  (song.primary_genres.music_genre_list[0].music_genre.music_genre_name) : (<span>Not available</span>) 
+                track.primary_genres.music_genre_list.length !== 0 ?
+                  (track.primary_genres.music_genre_list[0].music_genre.music_genre_name) : (<span>Not available</span>); 
               }
             </li>
             <li className="list-group-item">
-              <strong>Explicit Words</strong>: {song.explicit === 0 ? 'No' : 'Yes'}
+              <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
             </li>
             <li className="list-group-item">
-              <strong>Release Date</strong>: <Moment format="YYYY">{song.first_release_date}</Moment>
+              <strong>Release Date</strong>: <Moment format="YYYY">{track.first_release_date}</Moment>
             </li>
           </ul>
           <Link to="/" className="btn btn-dark btn-sm mb-5">Go back</Link>
         </React.Fragment>
-      )
+      );
     }
   }
 }
 
-export default Lyrics
+export default Lyrics;
