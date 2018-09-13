4490287008a97425044eaa8c8c72a5243637da22
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-13 12:01:41 -0700

    artists ready to display top 10 artists

--- src/components/artists/Artists.js
+++ src/components/artists/Artists.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import { Consumer } from '../../context';
 import Spinner from '../layout/Spinner';
 import Artist from './Artist';
 
+const rootURL = `http://ws.audioscrobbler.com/2.0/`;
+const key = process.env.REACT_APP_LAST_FM_KEY;
+
 class Artists extends Component {
+  state = {
+    topArtists: [],
+  }
+
+  componentDidMount() {
+    axios.get(`${rootURL}?method=chart.gettopartists&limit=10&api_key=${key}&format=json`)
+      .then(res => {
+        this.setState({ topArtists: res.data.artists.artist })
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
-    	<Consumer>
-    		{value => {
-    			const { trackInfo, heading } = value;
-    			
-          if (trackInfo === undefined || trackInfo.length === 0) {
-            return <Spinner />
-          } else {
-            return (
-              <React.Fragment>
-                <h2 className="text-center mb-4">{heading}</h2>
-                <div className="row">
-                  {trackInfo.map(item => (
-                    <Artist key={item.mbid} item={item} />
-                  ))}
-                </div>
-              </React.Fragment>
-            )
-          }
-    		}}
-    	</Consumer>
-    );
+      <React.Fragment>
+        <h2 className="text-center mb-4">Top 10 Artists</h2>
+        <div className="row">
+          {this.state.topArtists.map(item => (
+            <Artist key={item.mbid} item={item} />
+          ))}
+        </div>
+      </React.Fragment>
+    );     
   }
 }
 
