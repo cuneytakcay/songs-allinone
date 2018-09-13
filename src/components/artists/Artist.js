1c9795590e569887062fef315cd5dd6627c56615
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-13 13:11:32 -0700

    top tracks show on top artist

--- src/components/artists/Artist.js
+++ src/components/artists/Artist.js
@@ -9,24 +9,23 @@ const Artist = (props) => {
 			<div className="card mb-3 shadow-sm">
 				<div className="card-body row">
 					<div className="col-lg-6">
-						<Link to={`/artist/${item.name}`}>
-							<img src={item.image[3]['#text']} alt={item.name} className="w-100" />
+						<Link to={`/artist/${item.track[0].artist.name}`}>
+							<img src={item.track[0].image[3]['#text']} alt={item.track[0].artist.name} className="w-100" />
 						</Link>
 					</div>
 					<div className="col-lg-6 pt-2">
-						<Link to={`/artist/${item.name}`}>
-							<h5>{item.name}</h5>
+						<Link to={`/artist/${item.track[0].artist.name}`}> 
+							<h5><i className="fas fa-microphone-alt text-dark" /> &nbsp;{item.track[0].artist.name.toUpperCase()}</h5>
 						</Link>
-						<p className="card-text">
-							<i className="fas fa-play text-primary"></i> <strong>Track</strong>:
-							<br />
-							<i className="fas fa-compact-disc text-primary"></i> <strong>Album</strong>: 
-						</p>
+						<strong>Top tracks: </strong>
+						<ul>
+							{item.track.map(track => <li key={track.mbid || track.name}>{track.name}</li>)}
+						</ul>
 					</div>
 				</div>
 				<div className="pl-3 pr-3 pb-3">
-					<Link to={`#`} className="btn btn-dark btn-block">
-						<i className="fas fa-chevron-right text-primary"></i> View Lyrics
+					<Link to={`/artist/${item.track[0].artist.name}`} className="btn btn-dark btn-block">
+						<i className="fas fa-chevron-right text-primary" /> View Artist Info
 					</Link>
 				</div>
 			</div>
--- src/components/artists/Artists.js
+++ src/components/artists/Artists.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
-import { Consumer } from '../../context';
-import Spinner from '../layout/Spinner';
 import Artist from './Artist';
 
 const rootURL = `http://ws.audioscrobbler.com/2.0/`;
 const key = process.env.REACT_APP_LAST_FM_KEY;
+const topTracks = [];
 
 class Artists extends Component {
   state = {
     topArtists: [],
+    topTracks: [],
   }
 
   componentDidMount() {
     axios.get(`${rootURL}?method=chart.gettopartists&limit=10&api_key=${key}&format=json`)
       .then(res => {
-        this.setState({ topArtists: res.data.artists.artist })
+        this.setState({ topArtists: res.data.artists.artist });
+        this.state.topArtists.forEach(artist => {
+          this.getTopTracks(artist.name);
+        });
       })
       .catch(err => console.log(err));
+  }
+
+  getTopTracks = artist => {
+    axios.get(`${rootURL}?method=artist.gettoptracks&artist=${artist}&limit=3&api_key=${key}&format=json`)
+      .then(res => {
+        topTracks.push(res.data.toptracks);
+        this.setState({ topTracks: topTracks });
+      });
   }
 
   render() {
@@ -27,8 +37,8 @@ class Artists extends Component {
       <React.Fragment>
         <h2 className="text-center mb-4">Top 10 Artists</h2>
         <div className="row">
-          {this.state.topArtists.map(item => (
-            <Artist key={item.mbid} item={item} />
+          {this.state.topTracks.map(item => (
+            <Artist key={item.track[0].artist.mbid} item={item} />
           ))}
         </div>
       </React.Fragment>
